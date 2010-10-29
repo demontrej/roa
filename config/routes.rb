36f66eb7300ca6f293f3dc5292d808f99c35252a
juanca <juanc.com@gmail.com>
2010-10-28 21:01:26 -0400

    laptop 28/10

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :user_sessions
   map.login 'login', :controller => 'user_sessions', :action => 'new'
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
-  map.resources :users
+  map.resources :users, :collection => {:nuevo_usuario => :get}
   #collection que lo referencia a algo general 
   map.resources :libros, :collection => {:nuevo => :get}
 
